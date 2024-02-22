8e527c81d04a4fe022e4a241f7297cbc38c596e6
Alex Lepin <alexlepin@mail.ru>
2024-02-22 20:07:14 +0300

    feat: create temperature converter component

--- react-vite-project/src/App.jsx
+++ react-vite-project/src/App.jsx
@@ -1,16 +1,18 @@
 import "./App.css";
-import CommentsList from "./components/CommentsList/CommentsList";
-import Message from "./components/Message/Message";
+import TemperatureConverter from "./components/TemperatureConverter";
+// import CommentsList from "./components/CommentsList/CommentsList";
+// import Message from "./components/Message/Message";
 
 export default function App() {
   return (
     <>
-      <Message>Lorem ipsum dolor sit amet.</Message>
+      {/* <Message>Lorem ipsum dolor sit amet.</Message>
       <Message>
         Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam,
         ducimus.
       </Message>
-      <CommentsList></CommentsList>
+      <CommentsList></CommentsList> */}
+      <TemperatureConverter />
     </>
   );
 }
---
+++ react-vite-project/src/components/TemperatureConverter.jsx
@@ -0,0 +1,52 @@
+import { TextField, InputAdornment } from "@mui/material";
+import { useState } from "react";
+
+export default function TemperatureConverter() {
+  const [celsius, setCelsius] = useState(0);
+  const [fahrenheit, setFahrenheit] = useState(0);
+
+  function celsiusToFahrenheit(e) {
+    setCelsius(e.target.value);
+    setFahrenheit((e.target.value * 9) / 5 + 32);
+  }
+  function fahrenheitToCelsius(e) {
+    setFahrenheit(e.target.value);
+    setCelsius(((e.target.value - 32) * 5) / 9);
+  }
+
+  return (
+    <>
+      <TextField
+        variant="outlined"
+        label="Temperature in C"
+        helperText={
+          isNaN(celsius) || celsius === ""
+            ? "Enter the temperature in degrees Celsius"
+            : ""
+        }
+        style={{ marginRight: "20px" }}
+        value={celsius}
+        onChange={celsiusToFahrenheit}
+        error={isNaN(celsius)}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">C</InputAdornment>,
+        }}
+      />{" "}
+      <TextField
+        variant="outlined"
+        label="Temperature in F"
+        helperText={
+          isNaN(fahrenheit) || fahrenheit === ""
+            ? "Enter the temperature in degrees Fahrenheit"
+            : ""
+        }
+        value={fahrenheit}
+        onChange={fahrenheitToCelsius}
+        error={isNaN(fahrenheit)}
+        InputProps={{
+          startAdornment: <InputAdornment position="start">F</InputAdornment>,
+        }}
+      />
+    </>
+  );
+}
